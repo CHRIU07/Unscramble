5157536b369d8fc9dfcf50d8e52180da31f2d868
CHRI007 <afroilanjrchristopher@gmail.com>
2026-08-14 15:42:58 +0800

    Phase 1-5 Unscramble App

---
+++ app/src/main/java/com/example/todolistapp/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.example.todolistapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import com.example.todolistapp.ui.theme.ToDoListAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ToDoListAppTheme {
+                GameScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun GameScreen() {
+    var userAnswer by remember {
+        mutableStateOf("")
+    }
+
+    val words = listOf(
+        "CAT",
+        "DOG",
+        "BOOK"
+    )
+
+    var currentWordIndex by remember {
+        mutableStateOf(0)
+    }
+
+    val correctAnswer = words[currentWordIndex]
+
+    var score by remember {
+        mutableStateOf(0)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "UNSCRAMBLE",
+            fontSize = 30.sp
+        )
+
+        Text(
+            text = correctAnswer,
+            fontSize = 40.sp
+        )
+
+        Text(
+            text = "Unscramble the word!"
+        )
+
+        OutlinedTextField(
+            value = userAnswer,
+            onValueChange = {
+                userAnswer = it
+            },
+            label = {
+                Text("Enter your answer")
+            }
+        )
+
+        Button(
+            onClick = {
+                if (userAnswer == correctAnswer) {
+                    score++
+                    if (currentWordIndex < words.size - 1) {
+                        currentWordIndex++
+                        userAnswer = ""
+                    }
+                }
+            }
+        ) {
+            Text("SUBMIT")
+        }
+
+        Text(
+            text = "Score: $score"
+        )
+    }
+}
